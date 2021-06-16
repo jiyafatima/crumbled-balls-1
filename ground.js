f71f411b86d4490f294f5a3552424bbedd741bc8
jiyafatima <70758022+jiyafatima@users.noreply.github.com>
2021-06-16 18:04:47 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,22 @@
+class Ground{
+    constructor(x,y,height,width){
+        var options={
+            isStatic:true,
+            'restitution':0,
+            'friction':0,
+            density:1
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+
+        World.add(world,this.body);
+    }
+    display(){
+        rectMode(CENTER);
+
+        fill(255)
+
+        rect(this.body.position.y,this.width,this.height);
+    }
+}
